827539fc001e5ed384683694a371def41218103c
mayank <biostemp@outlook.com>
2023-12-27 19:11:38 +0530

    updated color scheme

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -33,13 +33,12 @@
             class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
         >
             <h2 class="font-semibold text-4xl sm:tet-5xl md:text-6xl">
-                Hi! I'm <span class="poppins text-violet-400">Mayank</span>
+                Hi! I'm <span class="poppins text-cyan-400">Mayank</span>
                 Rajput <br />Software Engineer
-                <span class="poppins text-violet-400"> Intern</span>
+                <span class="poppins text-cyan-400"> Intern</span>
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
-                My <span class="text-violet-400">favorite tech </span> includes Kotlin
-                , Java , Flutter and C++ , Firebase , MySQL.
+                My <span class="text-cyan-400">favorite tech </span> includes Kotlin, Java, Flutter and C++, Firebase, MySQL.
             </p>
             <a
                 class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
@@ -47,17 +46,17 @@
                 target="_blank"
             >
                 <div
-                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                    class="absolute top-0 right-full w-full h-full bg-cyan-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
                 />
                 <h4 class="relative z-9">Get in touch &rarr;</h4>
             </a>
         </div>
 
-        <div class="relative shadow-2xl grid place-items-center">
+        <div class="relative shadow-3xl grid place-items-center lg:float-right">
             <img
                 src={"images/pfp.png"}
                 alt="Profile Picture"
-                class="object-cover z-[2] max-h-[70vh] align"
+                class="object-cover z-[2] max-h-[70vh] lg:float-right"
             />
         </div>
     </section>
@@ -68,36 +67,35 @@
                 A few of my creative endeavors.
             </h6>
             <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                Curious to <span class="poppins text-violet-400">see</span> my work?
+                Curious to <span class="poppins text-cyan-400">see</span> my work?
             </h3>
         </div>
         <a
-            href="#"
+            href="https://gsap.com/docs/v3/Installation/guides/Club%20GSAP%20&%20Vercel"
             target="_blank"
-            class="mx-auto px-4 py-2 rounded-md border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+            class="mx-auto px-4 py-2 rounded-md border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 duration-200"
         >
-            <i class="fa-regularfa-circle-play" />
+            <i class="fa-regular fa-circle-play" />
             <p>Watch the video</p>
         </a>
-        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10 ">
             <Step step={steps[0]}>
                 <p>
                     This project Lorem Ispum qeador <strong
-                        class="text-violet-400">Firebase or highlighted</strong
-                    > bs ese hi paragraph bharna hai bhai.
+                    class="text-cyan-400">Firebase or highlighted</strong> bs ese hi paragraph bharna hai bhai.
                 </p>
             </Step>
             <Step step={steps[1]}>
                 <p>
                     This project Lorem Ispum qeador <strong
-                        class="text-violet-400">Firebase or highlighted</strong
+                        class="text-cyan-400">Firebase or highlighted</strong
                     > bs ese hi paragraph bharna hai bhai.
                 </p>
             </Step>
             <Step step={steps[2]}>
                 <p>
                     This project Lorem Ispum qeador <strong
-                        class="text-violet-400">Firebase or highlighted</strong
+                        class="text-cyan-400">Firebase or highlighted</strong
                     > bs ese hi paragraph bharna hai bhai.
                 </p>
             </Step>
@@ -108,11 +106,11 @@
         class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
     >
         <div
-            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-stone-500 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-stone-500 py-4"
         >
             <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
             <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                A bit <span class="poppins text-violet-400">about</span>
+                A bit <span class="poppins text-cyan-400">about</span>
             </h3>
         </div>
         <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
@@ -122,7 +120,7 @@
             {#each benefits as benefit, index}
                 <div class="flex gap-6 sm:gap-8">
                     <p
-                        class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                        class="poppins text-4xl sm:text-5xl md:text-6xl text-zinc-500 font-semibold"
                     >
                         0{index + 1}
                     </p>
